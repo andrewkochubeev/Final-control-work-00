726187c596944ba8006cadb9635294327493930a
Andrew_Kochubeeev <andrew-k-44@mail.ru>
2023-04-18 22:19:49 +0300

    Add InputStringArray method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из
+// имеющегося массива строк формирует новый
+// массив из строк, длина которых меньше, либо равна 3 символам.
+// Первоначальный массив можно ввести с клавиатуры, либо задать
+// на старте выполнения алгоритма. При решении не рекомендуется
+// пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string[] InputStringArray(int size)
+{
+    
+    string[] wordsArray= new string[size];
+    Console.WriteLine("Вводите слова");
+    for (int i = 0; i < size; i++)
+    {
+        wordsArray[i]=Console.ReadLine()!;
+    }
+    return wordsArray;
+}
+
+Console.Write("Введите количество слов в массиве: ");
+int sizeArr = Convert.ToInt32(Console.ReadLine());
+string[] wordsArr = InputStringArray(sizeArr);
