ac93db0467febcf0e62f5e120d228dabdc3d5f8d
Andrew_Kochubeeev <andrew-k-44@mail.ru>
2023-04-18 22:41:44 +0300

    Add SelectedWordsArray method

--- Program.cs
+++ Program.cs
@@ -7,16 +7,36 @@
 
 string[] InputStringArray(int size)
 {
-    
-    string[] wordsArray= new string[size];
+
+    string[] wordsArray = new string[size];
     Console.WriteLine("Вводите слова");
     for (int i = 0; i < size; i++)
     {
-        wordsArray[i]=Console.ReadLine()!;
+        wordsArray[i] = Console.ReadLine()!;
     }
     return wordsArray;
+}
+string[] SelectedWordsArray(string[] wordsArray)
+{
+    int resultArraySize = 0;
+    for (int i = 0; i < wordsArray.Length; i++)
+    {
+        if (wordsArray[i].Length <= 3) resultArraySize++;
+    }
+    string[] resultArray = new string[resultArraySize];
+    int j = 0;
+    for (int i = 0; i < wordsArray.Length; i++)
+    {
+        if (wordsArray[i].Length <= 3)
+        {
+            resultArray[j] = wordsArray[i];
+            j++;
+        }
+    }
+    return resultArray;
 }
 
 Console.Write("Введите количество слов в массиве: ");
 int sizeArr = Convert.ToInt32(Console.ReadLine());
 string[] wordsArr = InputStringArray(sizeArr);
+string[] resultArr = SelectedWordsArray(wordsArr);
